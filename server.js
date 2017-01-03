296980fcd1f0aeb24f6cee3549bfd5228d0bebe9
Julen Landa Alustiza <julen@zokormazo.info>
2017-01-03 00:44:27 +0000

    add /:str route logic

--- server.js
+++ server.js
@@ -2,4 +2,26 @@ var express = require('express')
 
 var app = express()
 
+app.get('/:str', function(req, res) {
+    var utc = parseInt(req.params.str)
+    var result;
+    if (utc)
+        utc *= 1000;
+    else
+        utc = Date.parse(req.params.str)
+
+    if (utc)
+        result = {
+            unix: Math.floor(utc/1000),
+            natural: new Date(utc).toDateString()
+        }
+    else
+        result = {
+            unix: null,
+            natural: null
+        }
+    res.send(result)
+    res.end()
+})
+
 app.listen('8080')
